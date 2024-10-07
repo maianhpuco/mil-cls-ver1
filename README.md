86207a1d7daea7b535b84d74c3bacd26870cdf72
Mai Anh Vu <maianhuel@gmail.com>
2024-10-07 13:43:29 -0500

    adding code to move came

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mil-cls-ver1
 MIL model (transformer + ssl) 
+```
+``` 
+echo "export PROJECT_DIR=$(pwd)" >> env/bin/activate 
+``` 
+
+```
+````
---
+++ src/pipelines/coscale.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+#preprocessing
+
+# feature learning Block
+
+class feature_learning:
+    def __init__(self):
+        pass 
+
+
+# Multiscale model 
+class Multiscale_model:
+    def __init__(self):
+        pass 
+    
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
